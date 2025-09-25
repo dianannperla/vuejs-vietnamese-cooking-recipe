f177a889432e8f1e396c4696d0496b1cb7cb2829
dianannperla <waafirmelyza@hotmail.com>
2025-09-25 15:37:52 +0100

    feat: initial commit for src/views/Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Công Thức Nấu Ăn Việt Nam</h1>
+    <p>Chào mừng bạn đến với trang nhà của ứng dụng công thức nấu ăn!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
